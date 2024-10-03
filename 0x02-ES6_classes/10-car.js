3ffb26b8e172d9f7640e8cbc2e75b02bcb158bb4
mumiak-mitch <akoth0mitch@gmail.com>
2024-10-03 17:06:07 +0000

    ES6 classes correction

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,14 +1,16 @@
-import Car from "./10-car.js";
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
 
-class TestCar extends Car {}
+  cloneCar() {
+    const clonedCar = new this.constructor[Symbol.species]();
+    return clonedCar;
+  }
 
-const tc1 = new TestCar("Nissan", "Turbo", "Pink");
-const tc2 = tc1.cloneCar();
-
-console.log(tc1);
-console.log(tc1 instanceof TestCar);
-
-console.log(tc2);
-console.log(tc2 instanceof TestCar);
-
-console.log(tc1 == tc2);
+  static get [Symbol.species]() {
+    return this;
+  }
+}
